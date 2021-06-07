3e875cf8d67c0ac44fadf6eac38c2224a7453629
HorvathGergo <hgergo19@gmail.com>
2021-06-07 16:39:27 +0200

    b6

--- src/Greet.java
+++ src/Greet.java
@@ -1,7 +1,11 @@
 public class Greet {
+
     public static void main(String[] args) {
         System.out.println("Frimmel Jakab");
         System.out.println("Greetings");
-	
+    }
+
+    public void greet(String parameter) {
+        System.out.println("Greetings for the " + parameter);
     }
 }
