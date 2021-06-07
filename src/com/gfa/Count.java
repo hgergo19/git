4d59156c0d3e13d4f7d26944285441cf8998c685
HorvathGergo <hgergo19@gmail.com>
2021-06-07 16:05:03 +0200

    b5

--- src/com/gfa/Count.java
+++ src/com/gfa/Count.java
@@ -2,7 +2,7 @@ package com.gfa;
 
 public class Count {
 
-    public void count(int myAge) {
-        System.out.println("My age is : " + myAge);
+    public void count(int myAge, int someOtherNumber) {
+        System.out.println("My age is : " + myAge + " a number : " + someOtherNumber);
     }
 }
