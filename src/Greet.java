5a14dcbc4bfb1d2685c0f6d07cf4c7e8a2ee1f74
Szabó Ákos <szabo.akos92@gmail.com>
2021-06-07 16:22:10 +0200

    c3

--- src/Greet.java
+++ src/Greet.java
@@ -1,7 +1,6 @@
 public class Greet {
     public static void main(String[] args) {
-        System.out.println("Frimmel Jakab");
-        System.out.println("Greetings");
+		System.out.println("Greetings");
 	
     }
 }
